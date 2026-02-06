1ff6cc78ce094b3f5c22beb6f2b98eb0ad518d99
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-06 03:00:06 +0000

    Deleting files

--- ContinueModel.py
+++
@@ -1,8 +0,0 @@
-class ContinueModel(nn.Module):
-  def __init__(self, inputsize, activation, hidden_size, hidden_dim):
-    super().__init__()
-    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    self.network = build_network(inputsize,1, activation, [hidden_size]*hidden_dim)
-
-  def forward(self, x):
-    return torch.distributions.Bernoulli(logits=self.network(x))
