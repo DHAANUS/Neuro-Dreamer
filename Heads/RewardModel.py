5422e026829ca066bea160161398d761a2346416
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-10 10:10:31 +0000

    RewardModel V2

--- Heads/RewardModel.py
+++ Heads/RewardModel.py
@@ -1,8 +1,11 @@
 class RewardModel(nn.Module):
-  def __init__(self, inputsize, activation, hidden_size, hidden_dim):
+  def __init__(self, latent_length, latent_classes, deterministic_size, activation, hidden_size, hidden_dim):
     super().__init__()
+    self.latent_size = latent_length*latent_classes
+    self.input_size = deterministic_size+self.latent_size
+    self.activation = activation
     self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    self.network = build_network(inputsize, 2 , activation, [hidden_size]*hidden_dim)
+    self.network = build_nn(self.inputsize, self.hidden_size,self.layer_size ,2, activation)
 
   def forward(self, x):
     out = self.network(x)
