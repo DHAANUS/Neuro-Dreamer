a9ffb292ce35d75ddeae4d313e5c6d84ace4cc65
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-10 10:26:16 +0000

    Actor V3

--- Heads/Actor.py
+++ Heads/Actor.py
@@ -4,7 +4,7 @@ from torch.distributions import OneHotCategorical
 class Actor(nn.Module):
   def __init__(self, latent_classes, latent_length, deterministic_size, actionspace, activation, hidden_size, layer_size):
     super().__init__()
-    self.latent_size = latent_classes+latent_length
+    self.latent_size = latent_classes*latent_length
     inputsize = deterministic_size+self.latent_size
     self.network = build_nn(
         inputsize,
