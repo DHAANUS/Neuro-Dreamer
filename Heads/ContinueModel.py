b83346e9377a9c9f68b9fab931f30174ff1811c7
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-15 08:37:10 +0000

    Continue model modified

--- Heads/ContinueModel.py
+++ Heads/ContinueModel.py
@@ -1,15 +1,13 @@
 class ContinueModel(nn.Module):
-  # def __init__(self, inputsize, activation, hidden_size, hidden_dim):
-    # super().__init__()
-    # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # self.network = build_nn(inputsize,1, activation, [hidden_size]*hidden_dim)
-  def __init__(self, latent_length, latent_classes, deterministic_size, activation, hidden_size, hidden_dim):
+  def __init__(self,  inputsize, config):
     super().__init__()
-    self.latent_size = latent_length*latent_classes
-    self.input_size = deterministic_size+self.latent_size
-    self.activation = activation
-    self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    self.network = build_nn(self.inputsize, self.hidden_size,self.layer_size ,1, activation)
+    self.config = config.dreamer.continuationModel
+    self.input_size = inputsize
+    self.network = build_nn(self.input_size,
+                            self.config.hiddenSize,
+                            self.config.hiddenLayers,
+                            1, 
+                            self.config.activation)
 
   def forward(self, x):
     return torch.distributions.Bernoulli(logits=self.network(x))
