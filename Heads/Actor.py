d71a0ec0c5bcbaded551386849200bb0d2f0ff15
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-15 07:21:46 +0000

    Actor modified wrt Gumbel_Softmax

--- Heads/Actor.py
+++ Heads/Actor.py
@@ -1,6 +1,8 @@
+from math import tau
 import torch
 import torch.nn as nn
 from torch.distributions import OneHotCategorical
+from torch.nn import functional as F
 class Actor(nn.Module):
   def __init__(self, latent_classes, latent_length, deterministic_size, actionspace, activation, hidden_size, layer_size):
     super().__init__()
@@ -13,5 +15,19 @@ class Actor(nn.Module):
         actionspace,
         activation
     )
-  def forward(self, x):
-    return OneHotCategorical(logits=self.network(x))
+  def forward(self, x, tau,training=False):
+    logits = self.network(x)
+    dist = OneHotCategorical(logits=logits)
+
+    if training:
+      action = F.gumbel_softmax(logits,
+                                tau=tau,
+                                hard=True)
+      action_index = action.argmax(dim=-1)
+    else:
+      action = dist.sample()
+      action_index = action.argmax(dim=-1)
+
+    log_prob = dist.log_prob(action_index)
+    entropy = dist.entropy()
+    return action, log_prob, entropy, logits
