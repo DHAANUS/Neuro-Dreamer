037592181080f51401b8706662cb1db57d97a412
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-19 16:17:40 +0000

    Updated with imports

---
+++ main.py
@@ -0,0 +1,83 @@
+
+import gym
+import gym_super_mario_bros
+import os
+import torch
+import argparse
+from nes_py.wrappers import JoypadSpace
+from gym_super_mario_bros.actions import RIGHT_ONLY
+
+
+from Utils.Utils import seeding, loadConfig, ensureParentFile, saveLoss, plotMetrics
+from EnvProperties.env import envWrapper, envPreproccessing, getEnvProperties
+from DreamerCore.CentralInitialization import CentralInitialization
+from DreamerCore.WorldModel import WorldModel
+from DreamerCore.BehaviourModel import BehaviorModel
+from DreamerCore.EnvInteraction import EnvironmentInteraction
+from DreamerCore.SaveCheckPoints import Savingcheckpoints
+from DreamerCore.LoadCheckPoints import LoadCheckpoints
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def main(configFile):
+  config = loadConfig(configFile)
+  seeding(config.seed)
+
+  runName = f'{config.envname}_{config.run_name}'
+  checkpointLoad = os.path.join(config.folderName.checkpointFolder, f'{runName}_{config.checkpointLoad}')
+  metricsFilename = os.path.join(config.folderName.metricsFolder, runName)
+  plotFilename = os.path.join(config.folderName.plotFolder, runName)
+  checkpointFilenameBase = os.path.join(config.folderName.checkpointFolder, runName)
+  videoFilenameBase = os.path.join(config.folderName.videoFolder, runName)
+
+  ensureParentFile(metricsFilename, plotFilename, checkpointFilenameBase, videoFilenameBase)
+  base_env = gym_super_mario_bros.make(config.envname)
+  base_env = JoypadSpace(base_env, RIGHT_ONLY)
+  env = envWrapper(envPreproccessing(
+      gym.wrappers.ResizeObservation(base_env, (64, 64))))
+  base_enveval = gym_super_mario_bros.make(config.envname)
+  base_enveval = JoypadSpace(base_enveval, RIGHT_ONLY)
+  enveval = envWrapper(envPreproccessing(
+      gym.wrappers.ResizeObservation(base_enveval, (64, 64))))
+
+  observation_shape , action_size = getEnvProperties(env)
+
+  print(f'Observation shape: {observation_shape}', f'Action size: {action_size}')
+
+  core = CentralInitialization(observation_shape, action_size, device, config)
+  worldmodel = WorldModel(core, config)
+  behaviour = BehaviorModel(core, config)
+  envinter = EnvironmentInteraction(core, config)
+  loadCheckpoints = LoadCheckpoints(core, worldmodel, behaviour, envinter, config)
+  if config.resume:
+    loadCheckpoints.loadCheckPoint(checkpointLoad)
+  saveCheckpoints = Savingcheckpoints(core, worldmodel, behaviour, envinter, config)
+
+  envinter.envInteraction(env, config.episodedbeforestart, seed=config.seed)
+
+  iterationsNum = config.gradientSteps // config.replayRatio
+  for _ in range(iterationsNum):
+    for _ in range(config.replayRatio):
+      sampleData = core.buffer.sample(config.batchSize, config.dreamer.batchlength)
+      initialStates, worldModelMetrics = worldmodel.train_world(sampleData)
+      behaviourMetrics = behaviour.train_behaviour(initialStates)
+      core.totalGradientSteps += 1
+
+      if core.totalGradientSteps % config.checkPointInterval == 0 and config.saveCheckpoints:
+        suffix = f'{core.totalGradientSteps/1000:.0f}k'
+        saveCheckpoints.saveCheckpoint(f'{checkpointFilenameBase}_{suffix}')
+        evalScore = envinter.envInteraction(enveval, config.numEvaluationEpisodes, seed=config.seed, evaluation=True, savevideo=True, fileName=f'{videoFilenameBase}_{suffix}')
+        print(f'Saved Checkpoint and Video to {suffix:>6} gradient steps. Evaluation Score: {evalScore:>0.2f}')
+
+    RecentScore = envinter.envInteraction(env, config.numInteractionEpisodes, seed=config.seed)
+    if config.saveMetrics:
+      metricBase = {'envSteps': core.totalEnvSteps,
+                    'gradientSteps': core.totalGradientSteps,
+                    'totalReward': RecentScore }
+      saveLoss(metricsFilename, metricBase | worldModelMetrics | behaviourMetrics)
+      plotMetrics(f'{metricsFilename}', savePath=f'{plotFilename}', title=f'{config.envname}')
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--config', type=str, default='config.yml')
+  main(parser.parse_args().config)
