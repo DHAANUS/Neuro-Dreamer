43eddf63b6c801e66ad26fde7115146584efd387
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-20 08:24:20 +0000

    Updated with get_activation

--- Models/Encoder.py
+++ Models/Encoder.py
@@ -1,24 +1,26 @@
 
 import torch
 import torch.nn as nn
+from Utils.Utils import get_activation
 class Encoder(nn.Module):
-  def __init__(self, inputshape, outputsize, activation):
+  def __init__(self, inputshape, outputsize, activation, config):
     super().__init__()
     self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
     channels, height, width = inputshape
     self.outputsize = outputsize
+    self.activation = get_activation(config.dreamer.encoder.activation)
     self.network = nn.Sequential(
         nn.Conv2d(channels, 16, kernel_size=4, stride=4, padding=1),
-        activation,
+        self.activation,
         nn.Conv2d(16,32, kernel_size=4, stride=2, padding=1),
-        activation,
+        self.activation,
         nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
-        activation,
+        self.activation,
         nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
-        activation,
+        self.activation,
         nn.Flatten(),
         nn.Linear(128*(height//16)*(width//16), outputsize),
-        activation
+        self.activation
     )
   def forward(self, x):
     return self.network(x).view(-1, self.outputsize)
