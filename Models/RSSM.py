b84e984f01c925df7b812b53a22e362d655fdc7f
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-02-20 09:10:50 +0000

    Updated with typos

--- Models/RSSM.py
+++ Models/RSSM.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.distributions import Normal
 from Utils.Utils import build_nn
+from Utils.Utils import get_activation
 class Recurrent_model(nn.Module):
   def __init__(self, action_dim, stochasticSize, deterministic_size, config):
     super().__init__()
