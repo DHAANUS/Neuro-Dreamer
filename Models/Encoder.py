780e2ab6167a731cdc2aa3940856d450476d8d5f
DHAANUS KARTHIKEYAN <dhaanusdhaanus2@gmail.com>
2026-07-22 00:04:15 +0000

    Encoder Altered based on the spatial data

--- Models/Encoder.py
+++ Models/Encoder.py
@@ -6,9 +6,12 @@ class Encoder(nn.Module):
   def __init__(self, inputshape, outputsize, activation, config):
     super().__init__()
     self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+    self.flatten = nn.Flatten()
     channels, height, width = inputshape
     self.outputsize = outputsize
     self.activation = get_activation(config.dreamer.encoder.activation)
+    self.linear = nn.Linear(config.dreamer.encoder.depth*8*(height//config.dreamer.encoder.stride ** 4)*(width//config.dreamer.encoder.stride ** 4), outputsize)
+
     self.network = nn.Sequential(
         nn.Conv2d(channels, config.dreamer.encoder.depth*1, config.dreamer.encoder.kernelSize, config.dreamer.encoder.stride, padding=1),
         self.activation,
@@ -18,9 +21,12 @@ class Encoder(nn.Module):
         self.activation,
         nn.Conv2d(config.dreamer.encoder.depth*4, config.dreamer.encoder.depth*8, config.dreamer.encoder.kernelSize, config.dreamer.encoder.stride, padding=1),
         self.activation,
-        nn.Flatten(),
-        nn.Linear(config.dreamer.encoder.depth*8*(height//config.dreamer.encoder.stride ** 4)*(width//config.dreamer.encoder.stride ** 4), outputsize),
-        self.activation
+        # nn.Flatten(),
+        # nn.Linear(config.dreamer.encoder.depth*8*(height//config.dreamer.encoder.stride ** 4)*(width//config.dreamer.encoder.stride ** 4), outputsize),
+        # self.activation
     )
   def forward(self, x):
-    return self.network(x).view(-1, self.outputsize)
+    spatial_feat = self.network(x)
+    flattened = self.flatten(spatial_feat)
+    output = self.activation(self.linear(flattened))
+    return output.view(-1, self.outputsize), spatial_feat
